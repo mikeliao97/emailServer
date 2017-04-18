7b5374db51ca8fc30c3dab3dd568f7fbb3af6bc8
Mike Liao <mikeliao97@gmail.com>
2017-04-17 21:59:38 -0700

    initalize email server

---
+++ index.js
@@ -0,0 +1,21 @@
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('pearljam@pro');
+var to_email = new helper.Email('mikeliao97@berkeley.edu');
+var subject = 'Please confirm to let us know';
+var content = new helper.Content('text/plain', 'Your friend bob invited you to pearljam, \
+Follow this link to redirect and signup http://pearljam.pro/dfdfasd');
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
+var sg = require('sendgrid')('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');
+process.env.SENDGRID_API_KEY
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
+
+sg.API(request, function(error, response) {
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
+});
